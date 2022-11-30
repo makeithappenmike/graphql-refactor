bf2cd5f90dfeac6e3b84f909126b18af676d3d80
MakeithappenMike <omichaelaaron@gmail.com>
2022-11-30 13:25:33 -0500

    adding git ignore and installing dependecies

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://makeithappenmike:13Benny!Buddy79*@cluster0.oj3hbc6.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
